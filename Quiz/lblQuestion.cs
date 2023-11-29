d17933d54c3488866c7a1175c952cf44596c6b7d
Arrow A <141832297+Arrowaaa@users.noreply.github.com>
2023-11-29 11:57:11 -0300

    Melhorias do quiz 29/11/23

---
+++ Quiz/lblQuestion.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz
+{
+    public partial class lblQuestion : Form
+    {
+        private List<Question> questions;
+        private int currentQuestion;
+        private int score;
+
+        public lblQuestion()
+        {
+            InitializeComponent();
+            InitializeQuestions();
+            currentQuestion = 0;
+            score = 0;
+            ShowQuestion(currentQuestion);
+        }
+
+
+        private void InitializeQuestions()
+        {
+            questions = new List<Question>
+            {
+                new Question("Qual é a capital da França?",
+                             new List<string> { "Madri", "Berlim", "Paris", "Londres" }, 3),
+                new Question("Quem descobriu a penicilina?",
+                             new List<string> { "Alexander Fleming", "Marie Curie", "Isaac Newton", "Albert Einstein" }, 1),
+                new Question("Quantos continentes existem?",
+                             new List<string> { "5", "6", "7", "8" }, 2)
+            };
+        }
+
+        private void ShowQuestion(int questionNumber)
+        {
+            if (questionNumber < questions.Count)
+            {
+                Question question = questions[questionNumber];
+                lblQuestionn.Text = question.QuestionText;
+                radioButton1.Text = question.Choices[0];
+                radioButton2.Text = question.Choices[1];
+                radioButton3.Text = question.Choices[2];
+                radioButton4.Text = question.Choices[3];
+            }
+            else
+            {
+               
+                ScoreForm scoreForm = new ScoreForm(score, questions.Count);
+                scoreForm.ShowDialog();
+            }
+        }
+
+
+        private void btnnNext_Click_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Botão Next clicado");
+
+            if (currentQuestion < questions.Count)
+            {
+                Question current = questions[currentQuestion];
+                int selectedAnswer = GetSelectedAnswer();
+
+                if (selectedAnswer == current.CorrectAnswer)
+                {
+                    score++;
+                }
+
+                currentQuestion++;
+                ShowQuestion(currentQuestion);
+            }
+            else
+            {
+                // Caso currentQuestion atinja o número total de questões
+                if (currentQuestion == questions.Count)
+                {
+                    // Esconder o Form1
+                    this.Hide();
+
+                    // Escolher o próximo formulário com base na pontuação
+                    Form nextForm = GetNextForm();
+                    nextForm.ShowDialog();
+
+                    // Exibir novamente o Form1 após a transição (opcional)
+                    this.Show();
+                }
+            }
+        }
+
+        private Form GetNextForm()
+        {
+            // Lógica para determinar o próximo formulário com base na pontuação
+            if (score >= 2)
+            {
+                return new lblQuestion3();
+            }
+            else if (score == 1)
+            {
+                return new lblQuestion2();
+            }
+            else
+            {
+                return new ScoreForm(score, questions.Count);
+            }
+
+        }
+
+        private int GetSelectedAnswer()
+        {
+            if (radioButton1.Checked) return 1;
+            else if (radioButton2.Checked) return 2;
+            else if (radioButton3.Checked) return 3;
+            else if (radioButton4.Checked) return 4;
+            else return 0; // Nenhum selecionado
+        }
+
+        public class Question
+        {
+            public string QuestionText { get; set; }
+            public List<string> Choices { get; set; }
+            public int CorrectAnswer { get; set; }
+
+            public Question(string questionText, List<string> choices, int correctAnswer)
+            {
+                QuestionText = questionText;
+                Choices = choices;
+                CorrectAnswer = correctAnswer;
+            }
+        }
+    }
+
+    public partial class ScoreForm : Form
+    {
+        private int finalScore;
+        private int totalQuestions;
+
+        public ScoreForm(int score, int total)
+        {
+            InitializeComponent();
+            finalScore = score;
+            totalQuestions = total;
+            DisplayScore();
+        }
+
+        private void DisplayScore()
+        {
+            lblScore.Text = ("Pontuação Final é: "  + finalScore + "/" + totalQuestions);
+        }
+    }
+}
---
+++ Quiz/lblQuestion3.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Quiz
+{
+    partial class lblQuestion3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestionn = new System.Windows.Forms.Label();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // lblQuestionn
+            // 
+            this.lblQuestionn.AutoSize = true;
+            this.lblQuestionn.BackColor = System.Drawing.Color.AliceBlue;
+            this.lblQuestionn.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestionn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblQuestionn.Location = new System.Drawing.Point(174, 9);
+            this.lblQuestionn.Name = "lblQuestionn";
+            this.lblQuestionn.Size = new System.Drawing.Size(473, 39);
+            this.lblQuestionn.TabIndex = 17;
+            this.lblQuestionn.Text = "Quantos continentes existem?";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.BackColor = System.Drawing.Color.White;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton4.Location = new System.Drawing.Point(12, 297);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(47, 35);
+            this.radioButton4.TabIndex = 16;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "8";
+            this.radioButton4.UseVisualStyleBackColor = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.BackColor = System.Drawing.Color.White;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(12, 246);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(47, 35);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "7";
+            this.radioButton3.UseVisualStyleBackColor = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.White;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(12, 193);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 35);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "6";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.White;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(12, 140);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 35);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "5";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // lblQuestion3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Quiz.Properties.Resources.Terra_planeta_água;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblQuestionn);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "lblQuestion3";
+            this.Text = "lblQuestion3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestionn;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
